fd3587909f7a98c6a91d4f930daccd24bc20e896
Escalion <escalion86@gmail.com>
2023-02-27 01:38:53 +0700

    Added yndex metrika

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ import StateLoader from '@components/StateLoader'
 import getServerSidePropsFunc from '@server/getServerSidePropsFunc'
 import ProductionBlock from '@blocks/ProductionBlock'
 import GalleryBlock from '@blocks/GalleryBlock'
+import Script from 'next/script'
 // import filteredEventsSelector from '@state/selectors/filteredEventsSelector'
 // import { useRecoilValue } from 'recoil'
 // import filteredDirectionsSelector from '@state/selectors/filteredDirectionsSelector'
@@ -77,6 +78,30 @@ export default function Home(props) {
     <>
       <Head>
         <title>{`Центр осознанных знакомств - "Половинка успеха"`}</title>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(92601771, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });`,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/92601771"
+              style="position:absolute; left:-9999px;"
+              alt=""
+            />
+          </div>
+        </noscript>
       </Head>
       <StateLoader className="max-h-screen" {...props}>
         <Header />
