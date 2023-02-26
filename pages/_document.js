8b392ca4ab746a3c2ef59b635d16d99eb234c79f
Escalion <escalion86@gmail.com>
2023-02-27 01:02:30 +0700

    Added yndex metrika

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import YandexMetrika from 'next-yandex-metrika'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
@@ -22,35 +23,17 @@ class MyDocument extends Document {
             href="https://cdn.jsdelivr.net/npm/@tailwindcss/custom-forms@0.2.1/dist/custom-forms.css"
             rel="stylesheet"
           />
+          <YandexMetrika
+            yid={YOUR_YM_ID}
+            clickmap={true / false}
+            trackLinks={true / false}
+            accurateTrackBounce={true / false}
+            webvisor={true / false}
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-                ym(92601771, "init", {
-                      clickmap:true,
-                      trackLinks:true,
-                      accurateTrackBounce:true
-                });`,
-            }}
-          />
-          <noscript>
-            <div>
-              <img
-                src="https://mc.yandex.ru/watch/92601771"
-                style="position:absolute; left:-9999px;"
-                alt=""
-              />
-            </div>
-          </noscript>
         </body>
       </Html>
     )
